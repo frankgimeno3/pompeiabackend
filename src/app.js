6517c068bbc0d4d43ba5b31bb69f8acb975dad8e
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-06 19:03:07 +0200

    backend funcional

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
-import notificationRoutes from './routes/notificaciones.routes.js';
-import ofertasRoutes from './routes/ofertas.routes.js';
+ 
 import cors from 'cors';
 import session from 'express-session'
 import cookieParser from 'cookie-parser'
@@ -26,8 +25,7 @@ app.use(express.json());
 // Configurar rutas
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
-app.use('/notificaciones', notificationRoutes);
-app.use('/ofertas', ofertasRoutes);
+ 
 app.use(
     session({
       secret: process.env.SESS_SECRET,
