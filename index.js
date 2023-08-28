1a34d1193e9d8e83503c2b7f0ac066d1bf42792e
Frank Gimeno <frankgimeno3@gmail.com>
2023-08-28 17:22:22 +0200

    retorno a punto anterior

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-import express from 'express';
-import connectDB from './db.js';
+// index.js
+// import app from "./app.js";
+import connectDB from "./db.js";
+
 const app = express();
-
-const PORT = process.env.PORT || 5000;
-
-// Connect to the MongoDB database
+ 
+// Conectarse a la base de datos de MongoDB
 connectDB();
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+// Iniciar el servidor
+app.listen(5000, () => {
+  console.log("Servidor iniciado en el puerto 5000");
 });
 
-// app.js ............................................................................................
-
- import cors from 'cors';
-import session from 'express-session';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-
-// Import your route modules here
+// app.js
+import express from 'express';
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import fileRoutes from './routes/file.routes.js';
-
  
+import cors from 'cors';
+import session from 'express-session'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+// app.js ............................................................................................
+
+
 app.set('trust proxy', 1);
 
 app.use(cors());
-
-// Configuring middlewares
+// Configurar middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(logger('dev'));
 
-// Configuring session
-app.use(
-  session({
-    secret: process.env.SESS_SECRET || 'your-default-secret',
-    resave: true,
-    saveUninitialized: false,
-    cookie: {
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-      secure: process.env.NODE_ENV === 'production',
-      httpOnly: true,
-      maxAge: 60000, // 60 * 1000 ms === 1 min
-    },
-  })
-);
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(logger("dev"));
 
-// Configuring routes
+
+// Configurar rutas
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.use('/files', fileRoutes);
 
-// export default app;
+app.use(
+    session({
+      secret: process.env.SESS_SECRET,
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: process.env.NODE_ENV === 'production',
+        httpOnly: true,
+        maxAge: 60000 // 60 * 1000 ms === 1 min
+      }
+    })
+  );
+// export default app 
