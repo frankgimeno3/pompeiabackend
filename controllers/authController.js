d99492689f82622716ad9dd775b9fe7909547a6c
Frank Gimeno <frankgimeno3@gmail.com>
2023-08-28 19:15:45 +0200

    authcontroller te indica error

--- controllers/authController.js
+++ controllers/authController.js
@@ -70,6 +70,6 @@ export const login = async (req, res) => {
 
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Ha ocurrido un error al iniciar sesión" });
+    res.status(500).json({ message: "Ha ocurrido un error al iniciar sesión", error });
   }
 };
